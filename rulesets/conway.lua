d95ae1e853c473f4566365a192a5344bf2bea391
book <81801780+imabook@users.noreply.github.com>
2025-09-03 22:42:23 +0200

    rework de conway
    rework de la logica para que sea mas modular y mas facil de modificar en otras cosas que no sea el conway normal

--- rulesets/conway.lua
+++ rulesets/conway.lua
@@ -4,33 +4,44 @@ local function wrap(value, max)
 	return ((value - 1) % max) + 1
 end
 
-function conwayStep(b, x, y)
-	local surround = 0
-	for i = -1, 1, 1 do
-		for j = -1, 1, 1 do
+local function checkNeighborhood(b, x, y)
+	-- define lo que es un vecindario
+	-- y calcula cuantas celulas hay al rededor de la central
+
+	local surround =
+	{ { 1, 1, 1 },
+		{ 1, 0, 1 },
+		{ 1, 1, 1 } }
+
+	local count = 0
+	local bounds = (#surround - 1) / 2
+
+	for i = -bounds, bounds, 1 do
+		for j = -bounds, bounds, 1 do
 			local x2, y2 = wrap(x + j, b.x), wrap(y + i, b.y)
-			surround = surround + (b.state[y2][x2] and 1 or 0)
+			count = count + (b.state[y2][x2] and 1 or 0) * surround[i + bounds + 1][j + bounds + 1]
 		end
 	end
 
+	return count
+end
+
+function conwayStep(b, x, y)
+	-- reglas del conway normal:
+	-- stable 2 vecinos
+	-- born 3 vecinos
+	-- resto cel muere o se queda muerta
+
+	local surround = checkNeighborhood(b, x, y)
+
 	b.neighbours[y][x] = surround
 
-	if b.state[y][x] then
-		-- esta viva
-		if surround < 3 then
-			b.nextState[y][x] = false
-		elseif surround > 4 then
-			b.nextState[y][x] = false
-		else
-			b.nextState[y][x] = true
-		end
+	if surround == 2 then
+		b.nextState[y][x] = b.state[y][x]
+	elseif surround == 3 then
+		b.nextState[y][x] = true
 	else
-		-- esta muerta
-		if surround == 3 then
-			b.nextState[y][x] = true
-		else
-			b.nextState[y][x] = false
-		end
+		b.nextState[y][x] = false
 	end
 end
 
